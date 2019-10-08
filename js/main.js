a6608728d24963aa718bc588214bbb685fe8480b
Kalispeller1488 <sinitsin.aleksey2003@yandex.ru>
2019-10-08 09:04:16 +0300

    few sections

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$('.img-wrap').slick({
+  slidesToShow: 3,
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>'
+});
