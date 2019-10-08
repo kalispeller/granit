5f0fc5f8460a1901ee4cd4b499dc086c9dd943e4
Kalispeller1488 <sinitsin.aleksey2003@yandex.ru>
2019-10-08 12:21:57 +0300

    720px responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $('.img-wrap').slick({
   slidesToShow: 3,
   prevArrow: '<button type="button" class="slick-prev"></button>',
-  nextArrow: '<button type="button" class="slick-next"></button>'
+  nextArrow: '<button type="button" class="slick-next"></button>',
+  // responsive: [
+  //   {
+  //     breakpoint: 992,
+  //     settings: {
+  //       slidesToShow: 2
+  //     }
+  //   }
+  // ],
+  adaptiveHeight: true
 });
